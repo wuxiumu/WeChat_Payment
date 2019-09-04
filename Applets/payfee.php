107720206cebaaf72b4e81233d504909665dccfb
天行健，地势坤 <wuxiumu@163.com>
2019-09-04 19:23:21 +0800

    Create payfee.php

---
+++ Applets/payfee.php
@@ -0,0 +1,21 @@
+<?php
+include 'WeixinPay.php';
+$appid='xxxxxxxxxxx';
+$openid= $_GET['id'];
+$mch_id='xxxxxxxx';
+$key='xxxxxxxxxxxxxxxxxxxxxxx';
+$out_trade_no = $mch_id. time();
+$total_fee = $_GET['fee'];
+if(empty($total_fee)) //押金
+{
+    $body = "充值押金";
+    $total_fee = floatval(99*100);
+}
+ else {
+     $body = "充值余额";
+     $total_fee = floatval($total_fee*100);
+ }
+$weixinpay = new WeixinPay($appid,$openid,$mch_id,$key,$out_trade_no,$body,$total_fee);
+$return=$weixinpay->pay();
+ 
+echo json_encode($return);
